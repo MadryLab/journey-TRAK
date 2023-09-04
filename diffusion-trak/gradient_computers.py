49fc58f256991ccf965527adffa813770b4d3bd5
Kristian Georgiev <krisgrg@mit.edu>
2023-09-04 10:47:48 -0400

    merge gradient computers

--- diffusion-trak/gradient_computers.py
+++ diffusion-trak/gradient_computers.py
@@ -6,7 +6,7 @@ from .utils import _accumulate_vectorize, get_num_params
 import torch
 
 
-class DiffusionGradientComputerFeaturizing(AbstractGradientComputer):
+class DiffusionGradientComputer(AbstractGradientComputer):
     def __init__(self,
                  model: torch.nn.Module,
                  task: AbstractModelOutput,
@@ -15,6 +15,7 @@ class DiffusionGradientComputerFeaturizing(AbstractGradientComputer):
         self.model = model
         self.num_params = get_num_params(self.model)
         self.load_model_params(model)
+        self._are_we_featurizing = False
 
     def load_model_params(self, model) -> None:
         """ Given a a torch.nn.Module model, inits/updates the (functional)
@@ -35,10 +36,16 @@ class DiffusionGradientComputerFeaturizing(AbstractGradientComputer):
         """ Uses functorch's :code:`vmap` (see
         https://pytorch.org/functorch/stable/generated/functorch.vmap.html#functorch.vmap
         for more details) to vectorize the computations of per-sample gradients.
+        """
+        if self._are_we_featurizing:
+            return self._compute_per_sample_grad_featurizing(batch)
+        else:
+            return self._compute_per_sample_grad_scoring(batch)
 
-        Doesn't use :code:`batch_size`; only added to follow the abstract method
-        signature.
-
+    def _compute_per_sample_grad_featurizing(self,
+                                             batch: Iterable[Tensor],
+                                             ) -> Tensor:
+        """
         Args:
             batch (Iterable[Tensor]):
                 contains [data, labels, timesteps]
@@ -73,70 +80,10 @@ class DiffusionGradientComputerFeaturizing(AbstractGradientComputer):
                                   arr=grads)
         return grads
 
-    def compute_loss_grad(self, batch: Iterable[Tensor]) -> Tensor:
-        """Computes the gradient of the loss with respect to the model output
-
-        .. math::
-
-            \\partial \\ell / \\partial \\text{(model output)}
-
-        Note: For all applications we considered, we analytically derived the
-        out-to-loss gradient, thus avoiding the need to do any backward passes
-        (let alone per-sample grads). If for your application this is not feasible,
-        you'll need to subclass this and modify this method to have a structure
-        similar to the one of :meth:`FunctionalGradientComputer:.get_output`,
-        i.e. something like:
-
-        .. code-block:: python
-
-            grad_out_to_loss = grad(self.model_out_to_loss_grad, ...)
-            grads = vmap(grad_out_to_loss, ...)
-            ...
-
-        Args:
-            batch (Iterable[Tensor]):
-                batch of data
-
+    def _compute_per_sample_grad_scoring(self,
+                                         batch: Iterable[Tensor],
+                                         ) -> Tensor:
         """
-        return self.modelout_fn.get_out_to_loss_grad(self.model,
-                                                     self.func_weights,
-                                                     self.func_buffers,
-                                                     batch)
-
-
-class DiffusionGradientComputerScoringRandomNoise(AbstractGradientComputer):
-    def __init__(self,
-                 model: torch.nn.Module,
-                 task: AbstractModelOutput,
-                 grad_dim: int) -> None:
-        super().__init__(model, task, grad_dim)
-        self.model = model
-        self.num_params = get_num_params(self.model)
-        self.load_model_params(model)
-
-    def load_model_params(self, model) -> None:
-        """ Given a a torch.nn.Module model, inits/updates the (functional)
-        weights and buffers. See https://pytorch.org/docs/stable/func.html
-        for more details on :code:`torch.func`'s functional models.
-
-        Args:
-            model (torch.nn.Module):
-                model to load
-
-        """
-        self.func_weights = dict(model.named_parameters())
-        self.func_buffers = dict(model.named_buffers())
-
-    def compute_per_sample_grad(self,
-                                batch: Iterable[Tensor],
-                                ) -> Tensor:
-        """ Uses functorch's :code:`vmap` (see
-        https://pytorch.org/functorch/stable/generated/functorch.vmap.html#functorch.vmap
-        for more details) to vectorize the computations of per-sample gradients.
-
-        Doesn't use :code:`batch_size`; only added to follow the abstract method
-        signature.
-
         Args:
             batch (Iterable[Tensor]):
             [images, labels, timesteps]
@@ -169,10 +116,8 @@ class DiffusionGradientComputerScoringRandomNoise(AbstractGradientComputer):
 
         for i in range(n_iters):
             noise = torch.randn(images[:, 0].shape, device='cuda')
-
             # map over batch dimensions (hence 0 for each batch dimension, and None for model params)
             # batch_size x num_timesteps x dim, ordered x_0 to x_T
-
             _accumulate_vectorize(g=torch.func.vmap(grads_loss,
                                                     in_dims=(None, None, None, None, *([0] * 3)),
                                                     randomness='different')(self.model,
